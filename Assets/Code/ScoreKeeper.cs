ce0c238944c1c9e24adadd97406dd4d1f181a164
Darian Liang <darianliang2027@u.northwestern.edu>
2024-12-06 14:20:09 -0600

    Fixed textmp component of score keeping

--- Assets/Code/ScoreKeeper.cs
+++ Assets/Code/ScoreKeeper.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreKeeper : MonoBehaviour
 {
-    public Text scoreText;
+    public TextMeshProUGUI scoreText;
     public static int scoreCount;
     // Start is called before the first frame update
     void Start()
